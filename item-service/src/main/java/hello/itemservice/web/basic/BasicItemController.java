5c8aec08ee654f3badf99fcfc04b35f182811539
wonee <hwang9821@icloud.com>
2022-04-08 17:48:41 +0900

    add : thymeleaf file add

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -1,10 +1,15 @@
 package hello.itemservice.web.basic;
 
+import hello.itemservice.domain.Item;
 import hello.itemservice.domain.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -13,6 +18,17 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
 
-
+    //테스트 용
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
 }
