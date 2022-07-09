d28e085ac0fa5ffbe166aa76de82de4fb38a9391
unknown <woneee99@gmail.com>
2022-07-09 19:25:24 +0900

    chore: 프로젝트 설정

---
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,61 @@
+package com.cos.security1.controller;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user){
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encodePassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encodePassword);
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+
+}
