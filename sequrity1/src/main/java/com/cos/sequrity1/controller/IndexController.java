a50e2ce8782c783c831b116d596f8b72746fb9bf
unknown <woneee99@gmail.com>
2022-07-09 18:13:07 +0900

    chore : 스프링 시큐리티 설정

--- sequrity1/src/main/java/com/cos/sequrity1/controller/IndexController.java
+++ sequrity1/src/main/java/com/cos/sequrity1/controller/IndexController.java
@@ -1,4 +1,44 @@
 package com.cos.sequrity1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료되었습니다.";
+    }
 }
